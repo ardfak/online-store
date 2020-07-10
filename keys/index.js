f149a9d40c13ce90f80e557302748245c8d49bc2
ardfak <59231958+ardfak@users.noreply.github.com>
2020-07-10 11:40:17 +0300

    deployment preparation

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  MONGODB_URI:
-    'mongodb+srv://bagavdin:wrwstling@cluster0-3teno.mongodb.net/shop',
-  SESSION_SECRET: 'ardfak',
-  EMAIL_FROM: 'ardfak@mail.ru',
-  BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
